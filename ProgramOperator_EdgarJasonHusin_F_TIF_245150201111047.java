0a2bec2c7d80a0585667303b32c6618c45ff68db
Edgar Jason Husin <181236125+EdgarJason@users.noreply.github.com>
2024-09-12 13:47:20 +0700

    Update ProgramOperator_EdgarJasonHusin_F_TIF_245150201111047.java

--- ProgramOperator_EdgarJasonHusin_F_TIF_245150201111047.java
+++ ProgramOperator_EdgarJasonHusin_F_TIF_245150201111047.java
@@ -3,27 +3,21 @@ import java.util.Scanner;
 public class ProgramOperator_EdgarJasonHusin_F_TIF_245150201111047 {
     public static void main(String[] args) {
             Scanner scan= new Scanner(System.in);
-            float x,y,a,b,c,d;
+            double x,y;
 
             //code penginputan
             System.out.print("Masukkan operator pertama : ");
-            x= scan.nextInt();
+            x= scan.nextDouble();
 
             System.out.print("Masukkan operator kedua : ");
-            y= scan.nextInt();
-
-            //operasi aritmatika
-            a=x+y;
-            b=x-y;
-            c=x*y;
-            d=x/y;
+            y= scan.nextDouble();
 
             //output
             System.out.println("================================");
-            System.out.printf("Hasil penjumlahan :%.2f\n ",a);
-            System.out.printf("Hasil pengurangan :%.2f\n ",b);
-            System.out.printf("Hasil perkalian :%.2f\n ",c);
-            System.out.printf("Hasil pembagian :%.2f\n ",d);
+            System.out.println("Hasil penjumlahan : "+(x+y));
+            System.out.println("Hasil pengurangan : "+(x-y));
+            System.out.println("Hasil perkalian : "+(x*y));
+            System.out.println("Hasil pembagian : "+(x/y));
             System.out.println("================================");
 
     }
