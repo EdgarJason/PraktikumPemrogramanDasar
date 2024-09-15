7587341950f96ee5fb731f88d183098f8b4799d2
Edgar Jason Husin <181236125+EdgarJason@users.noreply.github.com>
2024-09-16 00:44:03 +0700

    Update ProgramDayaListrik_EdgarJasonHusin_F_TIF_245150201111047.java

--- ProgramDayaListrik_EdgarJasonHusin_F_TIF_245150201111047.java
+++ ProgramDayaListrik_EdgarJasonHusin_F_TIF_245150201111047.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 
 public class ProgramDayaListrik_EdgarJasonHusin_F_TIF_245150201111047 {
     public static void main(String[] args) {
+
+        //Nama : Edgar Jason Husin
+        //NIM : 245150201111047
+        //Kelas : F - TIF
+
         //declare variabel
         int a,b,c,d,e,f,g;
         String x,y;
@@ -14,10 +19,10 @@ public class ProgramDayaListrik_EdgarJasonHusin_F_TIF_245150201111047 {
         System.out.println("PROGRAM PENGHITUNG PEMAKAIAN LISTRIK SEDERHANA");
         System.out.println("==========================================================");
 
-        System.out.print("Masukkan nama = ");
+        System.out.print("Masukkan nama                     = ");
         x = scan.nextLine();
 
-        System.out.print("Kelurahan     = ");
+        System.out.print("Kelurahan                         = ");
         y = scan.nextLine();
 
         //input integer
@@ -35,17 +40,17 @@ public class ProgramDayaListrik_EdgarJasonHusin_F_TIF_245150201111047 {
 
         //operasi penghitungan
         e = c*1000;
-        f = (e/10);
+        f = (e*d/100);
         g = e+f;
 
         //output
         System.out.println("=========================PLN JAVA=========================");
-        System.out.println("Nama = "+ x);
-        System.out.println("Kelurahan = "+y);
+        System.out.println("Nama                = "+ x);
+        System.out.println("Kelurahan           = "+y);
         System.out.println("Pemakaian bulan ini = "+(b-a)+" Kwh Meter");
-        System.out.println("Tarif listrik = Rp "+e+",-");
-        System.out.println("PPJ "+d+"% = Rp "+f+",-");
-        System.out.println("TOTAL = Rp "+g+",-");
+        System.out.println("Tarif listrik       = Rp "+e+",-");
+        System.out.println("PPJ "+d+"%             = Rp "+f+",-");
+        System.out.println("TOTAL               = Rp "+g+",-");
         System.out.println("==========================================================");
     }
 }
