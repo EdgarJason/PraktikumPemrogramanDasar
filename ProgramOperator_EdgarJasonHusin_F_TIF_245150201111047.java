64f951c81d9844bff4cec8b719915a555200f3c8
Edgar Jason Husin <181236125+EdgarJason@users.noreply.github.com>
2024-09-16 20:57:48 +0700

    Update ProgramOperator_EdgarJasonHusin_F_TIF_245150201111047.java

--- ProgramOperator_EdgarJasonHusin_F_TIF_245150201111047.java
+++ ProgramOperator_EdgarJasonHusin_F_TIF_245150201111047.java
@@ -2,23 +2,30 @@ import java.util.Scanner;
 
 public class ProgramOperator_EdgarJasonHusin_F_TIF_245150201111047 {
     public static void main(String[] args) {
+
+            //Nama : Edgar Jason Husin
+            //NIM : 245150201111047
+            //Kelas : F - TIF
+
             Scanner scan= new Scanner(System.in);
             double x,y;
 
             //code penginputan
+            System.out.println("===============================================");
+
             System.out.print("Masukkan operator pertama : ");
             x= scan.nextDouble();
 
-            System.out.print("Masukkan operator kedua : ");
+            System.out.print("Masukkan operator kedua   : ");
             y= scan.nextDouble();
 
             //output
-            System.out.println("================================");
+            System.out.println("===============================================");
             System.out.println("Hasil penjumlahan : "+(x+y));
             System.out.println("Hasil pengurangan : "+(x-y));
-            System.out.println("Hasil perkalian : "+(x*y));
-            System.out.println("Hasil pembagian : "+(x/y));
-            System.out.println("================================");
+            System.out.println("Hasil perkalian   : "+(x*y));
+            System.out.println("Hasil pembagian   : "+(x/y));
+            System.out.println("===============================================");
 
     }
 }
